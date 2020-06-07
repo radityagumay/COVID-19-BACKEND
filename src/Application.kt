d79e4a743d6df1c1427bb06e2a5b9175b7acdf7e
radityagumay <raditya.g@go-jek.com>
2020-06-07 21:19:44 +0700

    initial commit

---
+++ src/Application.kt
@@ -0,0 +1,85 @@
+package com.radityalabs
+
+import com.github.mustachejava.DefaultMustacheFactory
+import io.ktor.application.Application
+import io.ktor.application.ApplicationCall
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.apache.Apache
+import io.ktor.html.respondHtml
+import io.ktor.http.ContentType
+import io.ktor.mustache.Mustache
+import io.ktor.mustache.MustacheContent
+import io.ktor.response.respond
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import kotlinx.css.*
+import kotlinx.html.*
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+    install(Mustache) {
+        mustacheFactory = DefaultMustacheFactory("templates/mustache")
+    }
+
+    val client = HttpClient(Apache) {
+    }
+
+    routing {
+        get("/") {
+            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+        }
+
+        get("/html-dsl") {
+            call.respondHtml {
+                body {
+                    h1 { +"HTML" }
+                    ul {
+                        for (n in 1..10) {
+                            li { +"$n" }
+                        }
+                    }
+                }
+            }
+        }
+
+        get("/styles.css") {
+            call.respondCss {
+                body {
+                    backgroundColor = Color.red
+                }
+                p {
+                    fontSize = 2.em
+                }
+                rule("p.myclass") {
+                    color = Color.blue
+                }
+            }
+        }
+
+        get("/html-mustache") {
+            call.respond(MustacheContent("index.hbs", mapOf("user" to MustacheUser(1, "user1"))))
+        }
+    }
+}
+
+data class MustacheUser(val id: Int, val name: String)
+
+fun FlowOrMetaDataContent.styleCss(builder: CSSBuilder.() -> Unit) {
+    style(type = ContentType.Text.CSS.toString()) {
+        +CSSBuilder().apply(builder).toString()
+    }
+}
+
+fun CommonAttributeGroupFacade.style(builder: CSSBuilder.() -> Unit) {
+    this.style = CSSBuilder().apply(builder).toString().trim()
+}
+
+suspend inline fun ApplicationCall.respondCss(builder: CSSBuilder.() -> Unit) {
+    this.respondText(CSSBuilder().apply(builder).toString(), ContentType.Text.CSS)
+}
